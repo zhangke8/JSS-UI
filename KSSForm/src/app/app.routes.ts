dbfccfd70d2a3439cbf9a81d590e003eccee057a
KZ <19340295+zhangke8@users.noreply.github.com>
2018-10-12 16:03:54 -0400

    removing files

--- KSSForm/src/app/app.routes.ts
+++
@@ -1,9 +0,0 @@
-import {Routes} from '@angular/router';
-import {Home} from './home/home';
-
-
-export const rootRouterConfig: Routes = [
-  {path: '', redirectTo: 'home', terminal: true},
-  {path: 'home', component: Home},
-];
-
