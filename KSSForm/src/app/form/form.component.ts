a511410b433f70d96284bc072f66ac369ea82742
zhangke8 <19340295+zhangke8@users.noreply.github.com>
2018-11-28 12:52:56 -0500

    update

---
+++ KSSForm/src/app/form/form.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule }  from '@angular/common';
+
+
+@Component({
+  selector: 'app-form',
+  templateUrl: './form.component.html',
+  styleUrls: ['./form.component.css']
+})
+export class FormComponent implements OnInit {
+  productTypes = [];
+
+  constructor() {
+    this.productTypes = ["AB", "AB/SB", "AB/INFLATOR", "AB/SB/SW"];
+  }
+  public show1: boolean = false;
+  public show2: boolean = false;
+  public show3: boolean = false;
+
+  toggle1() {
+    this.show1 = !this.show1;
+  }
+
+  toggle2() {
+    this.show2 = !this.show2;
+  }
+
+  toggle3() {
+    this.show3 = !this.show3;
+  }
+
+  ngOnInit() {
+  }
+
+
+}
