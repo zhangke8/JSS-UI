079ef278d8411bdc32ec47e38b2933ed1c13e5fd
zhangke8 <19340295+zhangke8@users.noreply.github.com>
2018-12-10 12:34:05 -0500

    get request is working

--- JSSForm/src/app/DBConnection.js
+++ JSSForm/src/app/DBConnection.js
@@ -1,52 +1,52 @@
-// var Connection = require('tedious').Connection;
-// var Request = require('tedious').Request;
+var Connection = require('tedious').Connection;
+var Request = require('tedious').Request;
 
-// // Create connection to database
-// var config =
-// {
-//     userName: 'admin_login',
-//     password: 'Start@123',
-//     server: 'manutd8.database.windows.net',
-//     options:
-//     {
-//         database: 'TestSDB',
-//         encrypt: true
-//     }
-// }
-// var connection = new Connection(config);
+// Create connection to database
+var config =
+{
+    userName: 'admin_login',
+    password: 'Start@123',
+    server: 'manutd8.database.windows.net',
+    options:
+    {
+        database: 'TestSDB',
+        encrypt: true
+    }
+}
+var connection = new Connection(config);
 
-// // Attempt to connect and execute queries if connection goes through
-// connection.on('connect', function(err)
-//     {
-//         if (err)
-//         {
-//             console.log(err)
-//         }
-//         else
-//         {
-//             console.log("success");
-//             queryDatabase()
-//         }
-//     }
-// );
+// Attempt to connect and execute queries if connection goes through
+connection.on('connect', function(err)
+    {
+        if (err)
+        {
+            console.log(err)
+        }
+        else
+        {
+            console.log("success");
+            queryDatabase()
+        }
+    }
+);
 
-// function queryDatabase(){
-//     console.log('Reading rows from the Table...');
+function queryDatabase(){
+    console.log('Reading rows from the Table...');
 
-//     // Read all rows from table
-//     var request = new Request(
-//         'SELECT * FROM "User"',
-//         function(err, rowCount, rows)
-//         {
-//             console.log(rowCount + ' row(s) returned');
-//             process.exit();
-//         }
-//     );
+    // Read all rows from table
+    var request = new Request(
+        'SELECT * FROM "User"',
+        function(err, rowCount, rows)
+        {
+            console.log(rowCount + ' row(s) returned');
+            process.exit();
+        }
+    );
 
-//     request.on('row', function(columns) {
-//         columns.forEach(function(column) {
-//             console.log("%s\t%s", column.metadata.colName, column.value);
-//         });
-//     });
-//     connection.execSql(request);
-// }
+    request.on('row', function(columns) {
+        columns.forEach(function(column) {
+            console.log("%s\t%s", column.metadata.colName, column.value);
+        });
+    });
+    connection.execSql(request);
+}
--- JSSForm/src/app/server.js
+++ JSSForm/src/app/server.js
@@ -1,41 +1,41 @@
-// var express = require('express');
-// var bodyparser = require('body-parser');
-// var path = require('path');
-// var http = require('http');
-// var app = express();
-// var Connection = require('tedious').Connection;
-// var Request = require('tedious').Request;
+var express = require('express');
+var bodyparser = require('body-parser');
+var path = require('path');
+var http = require('http');
+var app = express();
+var Connection = require('tedious').Connection;
+var Request = require('tedious').Request;
 
-// // Create connection to database
-// var config =
-// {
-//     userName: 'admin_login',
-//     password: 'Start@123',
-//     server: 'manutd8.database.windows.net',
-//     options:
-//     {
-//         database: 'TestSDB',
-//         encrypt: true
-//     }
-// }
-// var connection = new Connection(config);
+// Create connection to database
+var config =
+{
+    userName: 'admin_login',
+    password: 'Start@123',
+    server: 'manutd8.database.windows.net',
+    options:
+    {
+        database: 'TestSDB',
+        encrypt: true
+    }
+}
+var connection = new Connection(config);
 
-// // Attempt to connect and execute queries if connection goes through
-// connection.on('connect', function(err)
-//     {
-//         if (err)
-//         {
-//             console.log(err)
-//         }
-//         else
-//         {
-//             console.log("success");
-//         }
-//     }
-// );
+// Attempt to connect and execute queries if connection goes through
+connection.on('connect', function(err)
+    {
+        if (err)
+        {
+            console.log(err)
+        }
+        else
+        {
+            console.log("success");
+        }
+    }
+);
 
 
-// // var api = require('./routes/api');
+// var api = require('./routes/api');
 
 // app.use(bodyparser.json());
 // app.use(bodyparser.urlencoded({extended: false}));
@@ -46,30 +46,34 @@
 // app.get('/index',function(req,res){
 //     res.sendFile(__dirname+'/dist/index.html')
 // });
+// User_NAME, Password 
+app.get('/', function(req, res){
+//   logger.info("Server Root");
+    var request = new Request(
+        'SELECT User_NAME, Password FROM "User"',
+        function(err, rowCount, rows)
+        {
+            if (rowCount == 0) {
+                console.log("empty");
+            }
+            console.log(rowCount + ' row(s) returned');
+        }
+    );
+    
 
-// app.get('/', function(req, res){
-// //   logger.info("Server Root");
-//     var request = new Request(
-//         'SELECT User_NAME, Password FROM "User"',
-//         function(err, rowCount, rows)
-//         {
-//             console.log(rowCount + ' row(s) returned');
-//             // process.exit();
-//         }
-//     );
+    request.on('row', function(columns) {
+        columns.forEach(function(column) {
+            console.log("%s\t%s", column.metadata.colName, column.value);
+        });
+    });
+    connection.execSql(request);
+  res.send("Server Root");
+});
 
-//     request.on('row', function(columns) {
-//         columns.forEach(function(column) {
-//             console.log("%s\t%s", column.metadata.colName, column.value);
-//         });
-//     });
-//     connection.execSql(request);
-//   res.send("Server Root");
-// });
-
+// SELECT User_NAME, Password FROM "User" WHERE USER_NAME == username AND Password == password
 // app.post('/', function(req, res) {
 //     var request = new Request(
-//         'SELECT User_NAME, Password FROM "User" WHERE User_NAME == username AND Password == password',
+//         'SELECT * FROM User',
 //         function(data, err) {
 //             console.log(data);
 //         }
@@ -77,12 +81,12 @@
 //     res.status(200).send(req.body.userName + req.body.Password);
 // })
 
-// // app.use('/',api);
-// /*
-//     Get port from environment and store in express
-// */ 
-// var port = process.env.PORT || '3000';
+// app.use('/',api);
+/*
+    Get port from environment and store in express
+*/ 
+var port = process.env.PORT || '3000';
 
 
-// //Listen on provided PORT
-// app.listen(port, () => console.log("Server is running"));
+//Listen on provided PORT
+app.listen(port, () => console.log("Server is running"));
